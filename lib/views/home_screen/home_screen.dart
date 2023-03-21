b636e881824d9fbe203f84cbdcaef847dfe859eb
DarkkLegion <arshad18syed@gmail.com>
2023-03-21 21:19:22 +0530

    styling home screen widgets

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -20,92 +20,106 @@ class HomeScreen extends StatelessWidget {
               height: 60,
               color: lightGrey,
               child: TextFormField(
-                decoration: const InputDecoration(
-                  suffixIcon: Icon(Icons.search),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  suffixIcon: const Icon(Icons.search),
                   filled: true,
                   fillColor: whiteColor,
                   hintText: searchAnything,
-                  hintStyle: TextStyle(color: textfieldGrey),
+                  hintStyle: const TextStyle(color: textfieldGrey),
                 ),
               ),
             ),
-            VxSwiper.builder(
-              aspectRatio: 16 / 9,
-              autoPlay: true,
-              enlargeCenterPage: true,
-              height: 150,
-              itemCount: slidersList.length,
-              itemBuilder: (context, index) {
-                return Image.asset(
-                  slidersList[index],
-                  fit: BoxFit.fill,
-                )
-                    .box
-                    .rounded
-                    .clip(Clip.antiAlias)
-                    .margin(const EdgeInsets.symmetric(horizontal: 8))
-                    .make();
-              },
-            ),
-            10.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                2,
-                (index) => homeButtons(
-                    height: context.screenHeight * 0.1,
-                    width: context.screenWidth / 2.5,
-                    icon: index == 0 ? icTodaysDeal : icFlashDeal,
-                    title: index == 0 ? todaysDeal : flashSale),
+            15.heightBox,
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      height: 150,
+                      itemCount: slidersList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          slidersList[index],
+                          fit: BoxFit.fill,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      },
+                    ),
+                    10.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                        2,
+                        (index) => homeButtons(
+                            height: context.screenHeight * 0.1,
+                            width: context.screenWidth / 2.5,
+                            icon: index == 0 ? icTodaysDeal : icFlashDeal,
+                            title: index == 0 ? todaysDeal : flashSale),
+                      ),
+                    ),
+                    10.heightBox,
+                    VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      height: 150,
+                      itemCount: secondSlidersList.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          secondSlidersList[index],
+                          fit: BoxFit.fill,
+                        )
+                            .box
+                            .rounded
+                            .clip(Clip.antiAlias)
+                            .margin(const EdgeInsets.symmetric(horizontal: 8))
+                            .make();
+                      },
+                    ),
+                    10.heightBox,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                        3,
+                        (index) => homeButtons(
+                            height: context.screenHeight * 0.1,
+                            width: context.screenWidth / 3.5,
+                            icon: index == 0
+                                ? icTopCategories
+                                : index == 1
+                                    ? icBrands
+                                    : icTopSeller,
+                            title: index == 0
+                                ? topCategories
+                                : index == 1
+                                    ? brand
+                                    : topSeller),
+                      ),
+                    ),
+                    20.heightBox,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: featuredCategories.text
+                          .color(darkFontGrey)
+                          .size(18)
+                          .fontFamily(semibold)
+                          .make(),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            10.heightBox,
-            VxSwiper.builder(
-              aspectRatio: 16 / 9,
-              autoPlay: true,
-              enlargeCenterPage: true,
-              height: 150,
-              itemCount: secondSlidersList.length,
-              itemBuilder: (context, index) {
-                return Image.asset(
-                  secondSlidersList[index],
-                  fit: BoxFit.fill,
-                )
-                    .box
-                    .rounded
-                    .clip(Clip.antiAlias)
-                    .margin(const EdgeInsets.symmetric(horizontal: 8))
-                    .make();
-              },
-            ),
-            10.heightBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                3,
-                (index) => homeButtons(
-                    height: context.screenHeight * 0.1,
-                    width: context.screenWidth / 3.5,
-                    icon: index == 0
-                        ? icTopCategories
-                        : index == 1
-                            ? icBrands
-                            : icTopSeller,
-                    title: index == 0
-                        ? topCategories
-                        : index == 1
-                            ? brand
-                            : topSeller),
-              ),
-            ),
-            20.heightBox,
-            Align(
-              alignment: Alignment.centerLeft,
-              child: featuredCategories.text
-                  .color(darkFontGrey)
-                  .size(18)
-                  .fontFamily(semibold)
-                  .make(),
             )
           ],
         ),
